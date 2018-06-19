a080c3f124bf4fb4c42f7bcdba9e983b423b4a49
Dan Rauch <drauch@hawk.iit.edu>
2018-06-19 14:12:20 -0500

    bulma

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const path = require('path')
+const axios = require('axios')
 const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', async (req, res) => await res.render('pages/index'))
+  .get('/hello', async (req, res) => {
+    res.send('Hey what\'s up?'); 
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
