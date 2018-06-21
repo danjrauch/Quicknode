f5d631e03283683fbf6a7f5be521539a05873868
Dan Rauch <drauch@hawk.iit.edu>
2018-06-21 10:57:24 -0500

    js optimization

--- index.js
+++ index.js
@@ -50,65 +50,65 @@ app.get('/pg', async function (req, res){
 // SF
 
 app.get('/sf', async function (req, res){
-  console.log('I\'m trying to get into sf'); 
   await sf.login('serviointeg@servio.org', 'dummyPwd123!AlkdkWcPZ6spOpNwuNWQnLI7J').then(
-    results => { console.log('Logged in'); } 
-  ); 
+    results => { console.log('Logged into Salesforce'); } 
+  )
 
-  await sf.insert('Clifford Estate').then(
-    results => {
-      console.log(results); 
-    }
-  ); 
+  // Promise.all
 
-  await sf.query().then(
-    results => { 
-      console.log(results); 
-      res.send(results); 
-      console.log('Found accounts');  
-    }
-  ); 
-});
+  // asynchonous 
+  await Promise.all([  
+    // sf.insert([]).then(
+    //   results => { res.send(results); console.log(results) }
+    // ),
+    sf.query().then(
+      results => { res.send(results); console.log(results) }
+    ), 
+    // sf.delete(['hey from hell', 'Hello from hell', 'New one']).then(
+    //   results => { res.send(results); console.log(results) } 
+    // )
+  ])
+})
 
 // SF
 
-// QB
+// QB   
 
 // INSERT YOUR CONSUMER_KEY AND CONSUMER_SECRET HERE
 
-const consumerKey = 'Q0GphSloikbyU7PzvX6waihOtcLR6tEUNJ748qEJdviaVxPmB0';
-const consumerSecret = 'YFjUCiUBqMzFEHb8VY6dzloAKEMFOgqUj32vKQJg';
+const consumerKey = 'Q0GphSloikbyU7PzvX6waihOtcLR6tEUNJ748qEJdviaVxPmB0'
+const consumerSecret = 'YFjUCiUBqMzFEHb8VY6dzloAKEMFOgqUj32vKQJg'
 
 app.get('/', function (req, res) {
   // res.redirect('/intuit');
-  res.render('pages/index.ejs');
+  res.render('pages/index.ejs')
 });
 
 app.get('/intuit', function (req, res) {
-  res.render('pages/intuit.ejs', { port: port, appCenter: QuickBooks.APP_CENTER_BASE });
+  res.render('pages/intuit.ejs', { port: port, appCenter: QuickBooks.APP_CENTER_BASE })
 });
 
 // OAUTH 2 makes use of redirect requests
 function generateAntiForgery (session) {
-  session.secret = csrf.secretSync();
-  return csrf.create(session.secret);
+  session.secret = csrf.secretSync()
+  return csrf.create(session.secret)
 };
 
 app.get('/requestToken', function (req, res) {
   var redirecturl = QuickBooks.AUTHORIZATION_URL +
     '?client_id=' + consumerKey +
     '&redirect_uri=' + 
-    encodeURIComponent('https://quicknode.herokuapp.com/auth/intuit/callback/') + // PROD
-    //encodeURIComponent('http://localhost:' + port + '/auth/intuit/callback/') +  // LOCAl Make sure this path matches entry in application dashboard
+    //encodeURIComponent('https://quicknode.herokuapp.com/auth/intuit/callback/') + // PROD
+    encodeURIComponent('http://localhost:' + port + '/auth/intuit/callback/') +  // LOCAl Make sure this path matches entry in application dashboard
     '&scope=com.intuit.quickbooks.accounting' +
     '&response_type=code' +
-    '&state=' + generateAntiForgery(req.session);
+    '&state=' + generateAntiForgery(req.session)
 
-  res.redirect(redirecturl);
+  res.redirect(redirecturl)
 });
 
 app.get('/auth/intuit/callback', function (req, res) {
-  var auth = (new Buffer(consumerKey + ':' + consumerSecret).toString('base64'));
+  var auth = (new Buffer(consumerKey + ':' + consumerSecret).toString('base64'))
 
   var postBody = {
     url: 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
@@ -120,13 +120,13 @@ app.get('/auth/intuit/callback', function (req, res) {
     form: {
       grant_type: 'authorization_code',
       code: req.query.code,
-      redirect_uri: 'https://quicknode.herokuapp.com/auth/intuit/callback/' //PROD
-      //redirect_uri: 'http://localhost:' + port + '/auth/intuit/callback/'  // LOCAL Make sure this path matches entry in application dashboard
+      //redirect_uri: 'https://quicknode.herokuapp.com/auth/intuit/callback/' //PROD
+      redirect_uri: 'http://localhost:' + port + '/auth/intuit/callback/'  // LOCAL Make sure this path matches entry in application dashboard
     }
   };
 
   request.post(postBody, function (e, r, data) {
-    var accessToken = JSON.parse(r.body);
+    var accessToken = JSON.parse(r.body)
 
     // save the access token somewhere on behalf of the logged in user
     var qbo = new QuickBooks(consumerKey,
@@ -138,21 +138,25 @@ app.get('/auth/intuit/callback', function (req, res) {
                              true, /* turn debugging on */
                              4, /* minor version */
                              '2.0', /* oauth version */
-                             accessToken.refresh_token /* refresh token */);
+                             accessToken.refresh_token /* refresh token */)
 
     // console.log(qbo.token); save the token here probably to postgres
 
-    // qbo.findAccounts(function (_, accounts) {
-    //   console.log('I\'m about to get the accounts'); 
-    //   accounts.QueryResponse.Account.forEach(function (account) {
-    //     console.log(account.Name);
-        
-    //   });
-    // });
+    qbo.findAccounts(async function (_, accounts) {
+      await sf.login('serviointeg@servio.org', 'dummyPwd123!AlkdkWcPZ6spOpNwuNWQnLI7J').then(
+        results => { console.log('Logged into Salesforce'); } 
+      )
+      await sf.insert(accounts.QueryResponse.Account.map((x) => x.Name)).then(
+        results => { console.log(results) }
+      )
+      // .forEach(function (account) {
+      //   console.log(account.Name);
+      // })
+    })
 
-  });
+  })
 
-  res.send('<!DOCTYPE html><html lang="en"><head></head><body><script>window.opener.location.reload(); window.close();</script></body></html>');
-});
+  res.send('<!DOCTYPE html><html lang="en"><head></head><body><script>window.opener.location.reload(); window.close();</script></body></html>')
+})
 
 // QB
--- sf.js
+++ sf.js
@@ -1,21 +1,28 @@
-const jsforce = require('jsforce');
+const jsforce = require('jsforce')
 
 module.exports = { 
   login: sfLogin, 
   query: sfQuery, 
-  insert: sfInsert
+  insert: sfInsert, 
+  delete: sfDelete
 }
 
-const conn = new jsforce.Connection();
+const conn = new jsforce.Connection()
 
 async function sfLogin(user, pwd){
-  return await conn.login(user, pwd); 
+  return await conn.login(user, pwd)
 }
 
 async function sfQuery(){
-  return await conn.query('SELECT Id, Name FROM Account'); 
+  return await conn.query('SELECT Id, Name FROM Account')
 }
 
-async function sfInsert(name){
-  return await conn.create('Account', {Name: name}); 
+async function sfInsert(names){
+  return await conn.sobject('Account').insertBulk(names.map((x) => { return {Name: x} })) // {Name: name}); 
+}
+
+async function sfDelete(names){
+  return await conn.sobject('Account')
+                   .find({ Name : { $in : names} })
+                   .destroy()
 }
